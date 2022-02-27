1307debeeefc4a85db3fc0be15f9183ab9c102c9
EvgeniyK96 <93151413+EvgeniyK96@users.noreply.github.com>
2022-02-27 19:28:29 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 let btn = document.querySelector('.btn');
 let form = document.querySelector('.form');
+let close = document.querySelector('.close');
 
 
 btn.addEventListener('click', function () {
-	form.classList.toggle('active');
+	form.classList.add('active');
 });
+close.addEventListener('click', function() {
+	form.classList.remove('active')
+})
