93e638c893e7d23d9421c07e07fd353cbdb14a34
EvgeniyK96 <93151413+EvgeniyK96@users.noreply.github.com>
2022-03-10 20:22:13 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-let btn = document.querySelector('.btn');
+let buttons = document.querySelectorAll('.btn');
 let form = document.querySelector('.form');
 let close = document.querySelector('.close');
 
-
+buttons.forEach(btn => {
 btn.addEventListener('click', function () {
 	form.classList.add('active');
 });
 close.addEventListener('click', function() {
 	form.classList.remove('active')
+})
 })
